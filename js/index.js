6abc6f3e40ca81df9e777bc4080ad3c79ae1b79c
jimmyjhickey <jimmyjhickey@gmail.com>
2019-01-26 11:58:28 -0600

    Added more interests and accordions

--- js/index.js
+++ js/index.js
@@ -58,3 +58,21 @@ contact.onclick = function () {
 	currently_visible = toggle_visbility("contact", currently_visible);
 	currently_italic = toggle_italic("option-contact", currently_italic);
 }
+
+
+
+//  accordion functionality lifted from https://www.w3schools.com/howto/howto_js_accordion.asp
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
